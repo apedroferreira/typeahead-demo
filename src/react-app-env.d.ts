aa91f224b1faccfb272332777b70d74a9d497261
Pedro Ferreira <tonipedrof@hotmail.com>
2021-02-23 00:12:27 +0000

    Separate type definitions

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="react-scripts" />
-
-import 'styled-components'
-
-declare module 'styled-components' {
-  export interface DefaultTheme {
-    font: {
-      body: Record<string, FlattenSimpleInterpolation>
-    }
-    color: Record<string, string>
-  }
-}
---
+++ src/types/styled-components.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    font: {
+      body: Record<string, FlattenSimpleInterpolation>
+    }
+    color: Record<string, string>
+  }
+}
